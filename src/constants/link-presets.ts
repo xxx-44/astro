8155bc75f74cfad908984a4461bea35c190878c1
shu <shu.l.iove.you31@gmail.com>
2024-07-23 21:56:15 +0900

    post add

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -13,7 +13,7 @@ export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
   },
   [LinkPreset.Archive]: {
     name: i18n(I18nKey.archive),
-    url: 'https://next-task-rust-iota.vercel.app/',
+    url: '/archive/',
     external: true,
     // url: '/archive/',
   },
--- src/content/posts/test.md
+++
@@ -1,9 +0,0 @@
----
-title: test
-published: 2024-06-28
-description: ''
-image: ''
-tags: [Test,Astro]
-category: 'Test'
-draft: false 
----
